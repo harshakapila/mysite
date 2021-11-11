d12238243ea8951ae3af5c61497e16e91b7ed0bc
harshakapila <hkapilarathna@gmail.com>
2021-11-11 10:54:12 +0530

    got the nav bar working

--- ads/urls.py
+++ ads/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from os import name
+from django.urls import path, reverse_lazy
 from .import views
 
 app_name ='ads'
 
 urlpatterns = [
     path('', views.adshomeview, name='adshomeview'),
+    path('', views.AdListView.as_view(), name='all'),
+    path('', views.ad_create, name='ad_create'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
 def adshomeview(request):
     return render(request, 'ads/ads_home.html')
-    
+
+class AdListView(CreateView):
+    pass
+
+def ad_create(request):
+    pass
